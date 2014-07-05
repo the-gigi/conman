29448a0fda162580bd48e9dfe32be76f9516a667
Gigi <gigi.sayfan@aclimalabs.com>
2014-07-04 22:35:56 -0600

    better management of local server using latest psutil

--- tests/etcd_test_util.py
+++ tests/etcd_test_util.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import time
 import psutil
+import six
 
 
 def start_local_etcd_server():
@@ -16,23 +17,20 @@ def start_local_etcd_server():
     p = subprocess.Popen('etcd',
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
-
     # Wait for etcd process to start
     while not is_local_etcd_running():
-        time.sleep(0.1)
-
-    print 'etcd running...'
+        time.sleep(1)
 
 
 def is_local_etcd_running():
     # Get non-zombie process names
-    names = [p.name for p in psutil.process_iter() if p.status != 'zombie']
-    if not 'etcd' in names:
+    etcd = [p for p in psutil.process_iter() if p.name() == 'etcd']
+    if not etcd:
         return False
 
-    # Sleep a little to make sure it can respond
-    time.sleep(0.5)
-    return True
+    etcd = etcd[0]
+
+    return etcd.status() == 'running'
 
 
 def kill_local_etcd_server():
@@ -40,7 +38,7 @@ def kill_local_etcd_server():
         return
 
     for p in psutil.process_iter():
-        if p.name == 'etcd' and p.status != 'zombie':
+        if p.name() == 'etcd' and p.status() != 'zombie':
             p.kill()
 
     # Wait for 10 seconds for process to die
@@ -60,7 +58,7 @@ def add_key(key, values):
     The values must be a dictionary, sub-keys will be created
     """
     assert isinstance(values, dict)
-    for name, value in values.iteritems():
+    for name, value in six.iteritems(values):
         if isinstance(value, str):
             value = "'{0}'".format(value)
         os.system('etcdctl set {0}/{1} {2}'.format(key, name, value))
